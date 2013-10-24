4f792ce6ad0291d4c005789d4403816bc37bc142
Timothy Lin <lzh9102@gmail.com>
2013-10-24 16:18:18 +0800

    Print message if message not found

--- battery.c
+++ battery.c
@@ -3,24 +3,32 @@
  */
 
 #include <stdio.h>
+#include <stdlib.h>
 #include "config.h"
+
+void error_exit(const char *msg)
+{
+	fprintf(stderr, msg);
+	exit(1);
+}
 
 int main(int argc, char const *argv[])
 {
 	int bat_full, bat_now;
 	FILE *f;
+	const char *battery_not_found_msg = "battery not found";
 
 	/* get battery full status */
 	f = fopen(PREFIX "_full", "r");
 	if (!f)
-		return 1;
+		error_exit(battery_not_found_msg);
 	fscanf(f, "%d", &bat_full);
 	fclose(f);
 
 	/* get battery current status */
 	f = fopen(PREFIX "_now", "r");
 	if (!f)
-		return 1;
+		error_exit(battery_not_found_msg);
 	fscanf(f, "%d", &bat_now);
 	fclose(f);
 
