53eed508ed2c329d3ddd8f2f4f3a8fe2e49c557e
Che-Huai Lin <lzh9102@gmail.com>
2014-02-28 15:19:29 -0500

    Print remaining time in verbose mode

--- battery.c
+++ battery.c
@@ -5,6 +5,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 #include "config.h"
 
 void error_exit(const char *msg)
@@ -25,6 +26,13 @@ long read_number_from_file(const char *filename)
 		fclose(f);
 	}
 	return result;
+}
+
+void format_remaining_time(char *buffer, int bufsize, int seconds)
+{
+	int hour = seconds / 3600;
+	int minute = (seconds%3600) / 60;
+	snprintf(buffer, bufsize, "%dh %dm", hour, minute);
 }
 
 int main(int argc, char *argv[])
@@ -56,7 +64,23 @@ int main(int argc, char *argv[])
 
 	int charge_percent = charge_now * 100 / charge_full;
 
-	printf("[%d%c]\n", charge_percent, is_charging ? '+' : '-');
+	if (opt_short) /* short format (for tmux, zsh, ...) */
+		printf("[%d%c]\n", charge_percent, is_charging ? '+' : '-');
+	else { /* detailed report */
+		/* charge level and status */
+		const char *status = is_charging ? "charging" : "discharging";
+		printf("%d%% (%s)\n", charge_percent, status);
+
+		/* compute remaining time if the battery is discharging */
+		long current_uA = read_number_from_file(BAT_PATH "current_now");
+		if (!is_charging && current_uA > 0) {
+			/* time = charge(uWh) * seconds_per_hour(s/h) / current(uA) seconds */
+			int remaining_seconds = charge_full * 3600 / current_uA;
+			char str_remaining_time[16];
+			format_remaining_time(str_remaining_time, 16, remaining_seconds);
+			printf("estimated remaining time: %s\n", str_remaining_time);
+		}
+	}
 
 	return 0;
 }
